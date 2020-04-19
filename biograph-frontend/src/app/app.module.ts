d8f114c2c090a11e7d871f4a3c45953cf9eb1168
Andrii Semchenko <a7chenko@gmail.com>
2020-04-19 14:11:14 +0300

    Add stub for main front-end page
    Also added some stub components

--- biograph-frontend/src/app/app.module.ts
+++ biograph-frontend/src/app/app.module.ts
@@ -1,16 +1,41 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import {AppComponent} from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {TabsComponent} from './components/tabs/tabs.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  // TODO asem add routes here as soon as other components will be created
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    FooterComponent,
+    TabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
