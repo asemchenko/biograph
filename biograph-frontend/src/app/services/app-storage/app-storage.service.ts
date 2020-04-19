4ebbe7aeb4338005fe0e6bedb70d80a42693f2d1
Andrii Semchenko <a7chenko@gmail.com>
2020-04-19 11:30:52 +0300

    Add services stub

---
+++ biograph-frontend/src/app/services/app-storage/app-storage.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+
+/**
+ * Is used to get/put smth to local storage
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class AppStorageService {
+
+  constructor() {
+  }
+}
