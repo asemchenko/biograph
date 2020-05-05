7c11f507395fc181d9ac9c84fadd6f96e7bb29cb
Andrii Semchenko <a7chenko@gmail.com>
2020-05-05 09:11:14 +0300

    Configure frontend deploy to firebase

--- biograph-frontend/src/environments/environment.prod.ts
+++ biograph-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // FIXME asem change to real server before deploy
-  BASE_URL: 'https://biograph.herokuapp.com'
+  BASE_URL: 'https://biograph-backend.herokuapp.com'
 };
