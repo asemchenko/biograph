cab1ae99881b6675eb2879cc4d6704bf7bf8c6c5
Andrii <a7chenko@gmail.com>
2020-05-09 15:03:52 +0300

    [temp] Add tags component autocomplete

--- biograph-frontend/src/app/components/events-page/event-dialog/tags/tags.component.ts
+++ biograph-frontend/src/app/components/events-page/event-dialog/tags/tags.component.ts
@@ -1,12 +1,12 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {TagService} from '../../../../services/tag/tag.service';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Tag} from '../../../../models/Tag';
 import {MatAutocomplete, MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
 import {FormControl} from '@angular/forms';
 import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import {MatChipInputEvent} from '@angular/material/chips';
-import {map, startWith, withLatestFrom} from 'rxjs/operators';
+import {map, startWith, tap, withLatestFrom} from 'rxjs/operators';
 
 @Component({
   selector: 'app-tags',
@@ -18,6 +18,7 @@ export class TagsComponent implements OnInit {
   tags$: Observable<Tag[]>;
   filteredTags$: Observable<Tag[]>;
   selectedTags: Tag[] = [];
+  selectedTags$: Observable<Tag[]>;
   tagFormControl = new FormControl();
   @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
@@ -29,24 +30,24 @@ export class TagsComponent implements OnInit {
 
   ngOnInit(): void {
     this.tags$ = this.tagService.getTagsOwnedByCurrentUser();
+    this.selectedTags$ = of(this.selectedTags);
     this.filteredTags$ = this.tagFormControl.valueChanges.pipe(
-      /*tap(value => {
-        console.log('[filteredTags$] Got value: ', value);
+      tap(value => {
+        console.log('[filteredTags$] 1 Got value: ', value);
       }),
-      filter(value => !!value),*/
       startWith(null),
       map(value => value ? value : ''),
       map(value => typeof value === 'string' ? value : value.name),
-      withLatestFrom(this.tags$),
-      map(([tagName, allTags]) => {
-        const remainedTags = this.getRemainedTags(allTags);
-        console.log('Got remained tags: ', remainedTags);
+      withLatestFrom(this.tags$, this.selectedTags$),
+      map(([tagName, allTags, selectedTags]) => {
+        const remainedTags = this.getRemainedTags(allTags, selectedTags);
         return tagName ? this.filter(tagName, remainedTags) : remainedTags;
       }),
     );
   }
 
   add(event: MatChipInputEvent): void {
+    // TODO asem implement creating new tags just by tapping it's name
     console.log('Got add event: ', event);
     /*const input = event.input;
     const value = event.value;
@@ -65,7 +66,6 @@ export class TagsComponent implements OnInit {
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
-    console.log('Got selected event: ', event);
     this.selectedTags.push(event.option.value);
     this.tagInput.nativeElement.value = '';
     this.tagFormControl.setValue(null);
@@ -74,11 +74,16 @@ export class TagsComponent implements OnInit {
   remove(tag: Tag) {
     const index = this.selectedTags.indexOf(tag);
     this.selectedTags.splice(index, 1);
+    // [dirty hack] just for showing autocomplete
+    const curValue = this.tagInput.nativeElement.value;
+    this.tagInput.nativeElement.value = '';
+    this.tagFormControl.setValue(null);
+    this.tagInput.nativeElement.value = curValue;
+    this.tagFormControl.setValue(curValue);
   }
 
-  private getRemainedTags(allTags: Tag[]) {
-    console.log('[getRemainedTags] allTags/selectedTags: ', allTags, this.selectedTags);
-    return allTags.filter(tag => !this.selectedTags.map(t => t.tagId).includes(tag.tagId));
+  private getRemainedTags(allTags: Tag[], selectedTags: Tag[]) {
+    return allTags.filter(tag => !selectedTags.map(t => t.tagId).includes(tag.tagId));
   }
 
   private filter(tagName: string, allTags: Tag[]) {
