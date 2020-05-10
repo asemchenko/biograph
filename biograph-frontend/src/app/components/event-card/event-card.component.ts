5417b8ddd9ea4ff7ceb38fffe808b04d709c49c3
Andrii <a7chenko@gmail.com>
2020-05-10 23:30:52 +0300

    Implements event-card component

--- biograph-frontend/src/app/components/event-card/event-card.component.ts
+++ biograph-frontend/src/app/components/event-card/event-card.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Event} from '../../models/Event';
 
 @Component({
   selector: 'app-event-card',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./event-card.component.less']
 })
 export class EventCardComponent implements OnInit {
+  @Input()
+  event: Event;
 
   constructor() {
   }
